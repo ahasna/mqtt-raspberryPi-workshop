3907043090585730b0702a9a9a73094a40d4212d
Asem Hasna <asem.hasna@gmail.com>
2021-10-17 14:53:43 +0200

    format doc

--- code/dashboard/js/app.js
+++ code/dashboard/js/app.js
@@ -1,95 +1,99 @@
 var reconnectTimeout = 2000;
-var app = angular.module('myApp', ['ngMaterial']);
-app.controller("myController", function ($scope,$http) {
-
-var g_t = new JustGage({
+var app = angular.module("myApp", ["ngMaterial"]);
+app.controller("myController", function ($scope, $http) {
+  var g_t = new JustGage({
     id: "gauge_temperature",
     value: 0,
     min: 0,
     max: 50,
     title: "Temperature",
-    label: "\xB0C"
+    label: "\xB0C",
   });
 
-var g_h = new JustGage({
+  var g_h = new JustGage({
     id: "gauge_humidity",
     value: 0,
     min: 0,
     max: 100,
     title: "Humidity",
-    label: "%RH"
+    label: "%RH",
   });
 
-//MQTT
-const mqtt_broker = "broker.mqttdashboard.com";
-const temp_topic = "redi-cisco-2019/t";
-const humidity_topic = "redi-cisco-2019/h";
-const light_topic = "redi-cisco-2019/light";
+  //MQTT
+  const mqtt_broker = "broker.mqttdashboard.com";
+  const temp_topic = "redi-cisco-2019/t";
+  const humidity_topic = "redi-cisco-2019/h";
+  const light_topic = "redi-cisco-2019/light";
 
-//### Snippet B1-1 here
-// Create a client instance
-client = new Paho.MQTT.Client(
-  mqtt_broker,
-  Number(8000),
-  "bb_" + parseInt(Math.random() * 100, 10)
-);
-// set callback handlers
-client.onConnectionLost = onConnectionLost;
-client.onMessageArrived = onMessageArrived;
-mqttConnect();
-function mqttConnect() {
-// connect the client
-client.connect({onSuccess:onConnect,onFailure:onFailure});}
-
-//### Snippet B1-2 here
-// called when the client connects
-function onConnect() {
-  // Once a connection has been made, make a subscription and send a message.
-  client.subscribe(temp_topic);
-  client.subscribe(humidity_topic);
-  client.subscribe(light_topic);
-}
-
-
-function onFailure() {
-  setTimeout(mqttConnect, reconnectTimeout);
-}
-
-
-// called when the client loses its connection
-function onConnectionLost(responseObject) {
-  if (responseObject.errorCode !== 0) {
-    console.log("onConnectionLost:"+responseObject.errorMessage);
+  //### Snippet B1-1 here
+  // Create a client instance
+  client = new Paho.MQTT.Client(
+    mqtt_broker,
+    Number(8000),
+    "bb_" + parseInt(Math.random() * 100, 10)
+  );
+  // set callback handlers
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+  mqttConnect();
+  function mqttConnect() {
+    // connect the client
+    client.connect({ onSuccess: onConnect, onFailure: onFailure });
   }
-  setTimeout(mqttConnect, reconnectTimeout);
-  connect();
-}
 
-//### Snippet B2 here
-function onMessageArrived(message) {
-    if (message.destinationName == temp_topic) {      g_t.refresh(message.payloadString);    }
-    if (message.destinationName == humidity_topic) {     g_h.refresh(message.payloadString);   }
+  //### Snippet B1-2 here
+  // called when the client connects
+  function onConnect() {
+    // Once a connection has been made, make a subscription and send a message.
+    client.subscribe(temp_topic);
+    client.subscribe(humidity_topic);
+    client.subscribe(light_topic);
+  }
+
+  function onFailure() {
+    setTimeout(mqttConnect, reconnectTimeout);
+  }
+
+  // called when the client loses its connection
+  function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+      console.log("onConnectionLost:" + responseObject.errorMessage);
+    }
+    setTimeout(mqttConnect, reconnectTimeout);
+    connect();
+  }
+
+  //### Snippet B2 here
+  function onMessageArrived(message) {
+    if (message.destinationName == temp_topic) {
+      g_t.refresh(message.payloadString);
+    }
+    if (message.destinationName == humidity_topic) {
+      g_h.refresh(message.payloadString);
+    }
     if (message.destinationName == light_topic) {
-        if (message.payloadString == "on") {             
-	    $scope.light = true;
-        } else {
-            $scope.light = false;
-        }   
-	console.log(message.payloadString);     }   $scope.$apply();  
-}
+      if (message.payloadString == "on") {
+        $scope.light = true;
+      } else {
+        $scope.light = false;
+      }
+      console.log(message.payloadString);
+    }
+    $scope.$apply();
+  }
 
-//### Snippet B3 here
-$scope.switchLight = function() {
-	if (!$scope.light) {
-	    message = new Paho.MQTT.Message("on");
-	} else {
-	    message = new Paho.MQTT.Message("off");
-	}
-        message.destinationName = light_topic;
+  //### Snippet B3 here
+  $scope.switchLight = function () {
+    if (!$scope.light) {
+      message = new Paho.MQTT.Message("on");
+    } else {
+      message = new Paho.MQTT.Message("off");
+    }
+    message.destinationName = light_topic;
 
-        client.send(message);
-};
-document.getElementById("lightTopic").innerHTML = light_topic;
-document.getElementById("tempTopic").innerHTML = temp_topic;
-document.getElementById("humidityTopic").innerHTML = humidity_topic;
+    client.send(message);
+  };
+  document.getElementById("lightTopic").innerHTML = light_topic;
+  document.getElementById("tempTopic").innerHTML = temp_topic;
+  document.getElementById("humidityTopic").innerHTML = humidity_topic;
 });
